31210c7c5ccaf5fe961c56faab183b75ae7d882c
Nick Martin <nickleomartin@gmail.com>
2020-05-19 15:19:56 +0200

    Add flags for discovery, selection & replication modes

--- conductor/__init__.py
+++ conductor/__init__.py
@@ -13,6 +13,18 @@ def main():
                         '--config',
                         help='Conductor config file',
                         required=True)
+    parser.add_argument('-d',
+                        '--discover',
+                        help='Flag for discovery mode',
+                        action='store_true')
+    parser.add_argument('-s',
+                        '--select',
+                        help='Flag for selection mode',
+                        action='store_true')
+    parser.add_argument('-r',
+                        '--replicate',
+                        help='Flag for replication mode',
+                        action='store_true')
 
     args = parser.parse_args()
     config_filepath = args.config
@@ -21,9 +33,23 @@ def main():
     runner.validate_configs()
     runner.validate_environments()
 
-    runner.discover()
-    runner.select()
-    runner.replicate()
+    at_least_one_required_args = [args.discover, args.select, args.replicate]
+
+    if all(arg is not False for arg in at_least_one_required_args):
+        error_message = (
+            'At least one of arg must be specified: '
+            '--discover, --select or --replicate'
+        )
+        raise ValueError(error_message)
+
+    if args.discover:
+        runner.discover()
+
+    if args.select:
+        runner.select()
+
+    if args.replicate:
+        runner.replicate()
 
 
 if __name__ == '__main__':
